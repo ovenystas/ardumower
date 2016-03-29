d979a8de3dd9e0718683ccfe36149c8c0d4c7250
Ove Nystås <ove.nystas@gmail.com>
2016-03-29 02:31:44 +0200

    Format and cleanup of sender

--- sender/RunningMedian.h
+++ sender/RunningMedian.h
@@ -1,9 +1,8 @@
-// running median filter   
+// running median filter
 //
 // usage:
 //   RunningMedian<unsigned int,32> myMedian;
 //   if (myMedian.getStatus() == myMedian.OK)  myMedian.getMedian(_median);
-
 
 #ifndef RunningMedian_h
 #define RunningMedian_h
@@ -21,115 +20,159 @@
 
 #include <inttypes.h>
 
-template <typename T, int N> class RunningMedian {
+template<typename T, int N> class RunningMedian
+{
 
-public:
+  public:
 
-    enum STATUS {OK = 0, NOK = 1};
-
-    RunningMedian() {
-        _size = N;
-        clear();
+    enum STATUS
+    {
+      OK = 0, NOK = 1
     };
 
-    void clear() {
-        _cnt = 0;
-        _idx = 0;
-    };
+    RunningMedian()
+    {
+      _size = N;
+      clear();
+    }
 
-    void add(T value) {
-        _ar[_idx++] = value;
-        if (_idx >= _size) _idx = 0; // wrap around
-        if (_cnt < _size) _cnt++;
-    };
+    void clear()
+    {
+      _cnt = 0;
+      _idx = 0;
+    }
 
-    STATUS getMedian(T& value) {
-        if (_cnt > 0) {
-            sort();
-            value = _as[_cnt/2];
-            return OK;
-        }
-        return NOK;
-    };
+    void add(T value)
+    {
+      _ar[_idx++] = value;
+      if (_idx >= _size)
+      {
+        _idx = 0; // wrap around
+      }
+      if (_cnt < _size)
+      {
+        _cnt++;
+      }
+    }
 
-    STATUS getAverage(float &value) {
-        if (_cnt > 0) {
-            float sum = 0;
-            for (uint8_t i=0; i< _cnt; i++) sum += _ar[i];
-            value = sum / _cnt;
-            return OK;
-        }
-        return NOK;
-    };
+    STATUS getMedian(T& value)
+    {
+      if (_cnt > 0)
+      {
+        sort();
+        value = _as[_cnt / 2];
+        return OK;
+      }
+      return NOK;
+    }
 
-    STATUS getAverage(uint8_t nMedians, float &value) {
-        if ((_cnt > 0) && (nMedians > 0))
+    STATUS getAverage(float &value)
+    {
+      if (_cnt > 0)
+      {
+        float sum = 0;
+        for (uint8_t i = 0; i < _cnt; i++)
         {
-            if (_cnt < nMedians) nMedians = _cnt;     // when filling the array for first time
-            uint8_t start = ((_cnt - nMedians)/2);
-            uint8_t stop = start + nMedians;
-            sort();
-            float sum = 0;
-            for (uint8_t i = start; i < stop; i++) sum += _as[i];
-            value = sum / nMedians;
-            return OK;
+          sum += _ar[i];
         }
-        return NOK;
+        value = sum / _cnt;
+        return OK;
+      }
+      return NOK;
     }
 
-    STATUS getHighest(T& value) {
-        if (_cnt > 0) {
-            sort();
-            value = _as[_cnt-1];
-            return OK;
+    STATUS getAverage(uint8_t nMedians, float &value)
+    {
+      if ((_cnt > 0) && (nMedians > 0))
+      {
+        if (_cnt < nMedians)
+        {
+          nMedians = _cnt;     // when filling the array for first time
         }
-        return NOK;
-    };
-
-    STATUS getLowest(T& value) {
-        if (_cnt > 0) {
-            sort();
-            value =  _as[0];
-            return OK;
+        uint8_t start = ((_cnt - nMedians) / 2);
+        uint8_t stop = start + nMedians;
+        sort();
+        float sum = 0;
+        for (uint8_t i = start; i < stop; i++)
+        {
+          sum += _as[i];
         }
-        return NOK;
-    };
-
-    unsigned getSize() {
-        return _size;
-    };
-
-    unsigned getCount() {
-        return _cnt;
+        value = sum / nMedians;
+        return OK;
+      }
+      return NOK;
     }
 
-    STATUS getStatus() {
-        return (_cnt > 0 ? OK : NOK);
-    };
+    STATUS getHighest(T& value)
+    {
+      if (_cnt > 0)
+      {
+        sort();
+        value = _as[_cnt - 1];
+        return OK;
+      }
+      return NOK;
+    }
 
-private:
+    STATUS getLowest(T& value)
+    {
+      if (_cnt > 0)
+      {
+        sort();
+        value = _as[0];
+        return OK;
+      }
+      return NOK;
+    }
+
+    unsigned getSize()
+    {
+      return _size;
+    }
+
+    unsigned getCount()
+    {
+      return _cnt;
+    }
+
+    STATUS getStatus()
+    {
+      return (_cnt > 0 ? OK : NOK);
+    }
+
+  private:
     uint8_t _size;
     uint8_t _cnt;
     uint8_t _idx;
     T _ar[N];
     T _as[N];
-    void sort() {
-        // copy
-        for (uint8_t i=0; i< _cnt; i++) _as[i] = _ar[i];
+    void sort()
+    {
+      // copy
+      for (uint8_t i = 0; i < _cnt; i++)
+      {
+        _as[i] = _ar[i];
+      }
 
-        // sort all
-        for (uint8_t i=0; i< _cnt-1; i++) {
-            uint8_t m = i;
-            for (uint8_t j=i+1; j< _cnt; j++) {
-                if (_as[j] < _as[m]) m = j;
-            }
-            if (m != i) {
-                T t = _as[m];
-                _as[m] = _as[i];
-                _as[i] = t;
-            }
+      // sort all
+      for (uint8_t i = 0; i < _cnt - 1; i++)
+      {
+        uint8_t m = i;
+        for (uint8_t j = i + 1; j < _cnt; j++)
+        {
+          if (_as[j] < _as[m])
+          {
+            m = j;
+          }
         }
-    };
+        if (m != i)
+        {
+          T t = _as[m];
+          _as[m] = _as[i];
+          _as[i] = t;
+        }
+      }
+    }
 };
 
 #endif
---
+++ sender/sender.cpp
@@ -0,0 +1,380 @@
+/*
+ Ardumower (www.ardumower.de)
+ Copyright (c) 2013-2014 by Alexander Grau
+ Copyright (c) 2013-2014 by Sven Gennat
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ Perimeter sender v2   (for details see   http://wiki.ardumower.de/index.php?title=Perimeter_wire  )
+ Requires: Perimeter sender PCB v1.0   ( https://www.marotronics.de/Perimeter-Sender-Prototyp )
+ */
+
+#include <Arduino.h>
+#include "TimerOne.h"
+#include "EEPROM.h"
+#include "RunningMedian.h"
+
+// #define USE_DEVELOPER_TEST    1      // uncomment for new perimeter signal test (developers)
+
+// --- MC33926 motor driver ---
+#define USE_DOUBLE_AMPLTIUDE    1         // uncomment to use +/- input voltage for amplitude (default),
+// comment to use only +input/GND voltage for amplitude
+
+#define PIN_IN1       9  // M1_IN1         (if using old L298N driver, connect this pin to L298N-IN1)
+#define PIN_IN2       2  // M1_IN2         (if using old L298N driver, connect this pin to L298N-IN2)
+#define PIN_PWM       3  // M1_PWM / nD2   (if using old L298N driver, leave open)
+#define PIN_ENABLE    5  // EN             (connect to motor driver enable)
+
+// motor driver fault pin
+#define PIN_FAULT     4  // M1_nSF
+#define USE_PERI_FAULT        0     // use pinFault for driver fault detection? (set to '0' if not connected!)
+
+// motor driver feedback pin (=perimeter open/close detection, used for status LED)
+#define USE_PERI_CURRENT      1     // use pinFeedback for perimeter current measurements? (set to '0' if not connected!)
+#define PIN_FEEDBACK A0  // M1_FB
+#define PERI_CURRENT_MIN    0.03     // minimum Ampere for perimeter-is-closed detection
+
+// ---- sender current control (via potentiometer) ----
+// sender modulates signal (PWM), based on duty-cycle set via this potentiometer
+#define PIN_POT      A3  // 100k potentiometer (current control)
+
+// ---- sender automatic standby (via current sensor for charger) ----
+// sender detects robot via a charging current through the charging pins
+#define USE_CHG_CURRENT       0     // use charging current sensor for robot detection? (set to '0' if not connected!)
+#define PIN_CHARGE_CURRENT    A2     // ACS712-05 current sensor OUT
+#define CHG_CURRENT_MIN   0.008      // minimum Ampere for charging detection
+
+// ---- sender status LED ----
+#define  PIN_LED 13  // ON: perimeter closed, OFF: perimeter open, BLINK: robot is charging
+
+// code version
+#define VER "600"
+
+// --------------------------------------
+
+volatile int step = 0;
+volatile boolean enableSender = true;
+
+uint8_t dutyPWM = 0;
+float chargeCurrent = 0;
+float periCurrentAvg = 0;
+float periCurrentMax = 0;
+int faults = 0;
+boolean isCharging = false;
+boolean stateLED = false;
+unsigned int chargeADCZero = 0;
+RunningMedian<unsigned int, 16> periCurrentMeasurements;
+RunningMedian<unsigned int, 96> chargeCurrentMeasurements;
+
+unsigned long nextTimeControl = 0;
+unsigned long nextTimeInfo = 0;
+unsigned long nextTimeToggleLED = 0;
+
+// must be multiple of 2 !
+// http://grauonline.de/alexwww/ardumower/filter/filter.html
+// "pseudonoise4_pw" signal (sender)
+
+#ifdef USE_DEVELOPER_TEST
+// a more motor driver friendly signal (sender)
+int8_t sigcode[] =
+{
+  1, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+  -1, 0, 0, 0, 0, 1, 0, 0, 0, 0
+};
+#else
+int8_t sigcode[] =
+{
+  1,  1, -1, -1,  1, -1,  1, -1, -1,  1, -1,  1,
+  1, -1, -1,  1, -1, -1,  1, -1, -1,  1,  1, -1
+};
+#endif
+
+
+void timerCallback(void)
+{
+  if (enableSender)
+  {
+    if (sigcode[step] == 1)
+    {
+      digitalWrite(PIN_IN1, LOW);
+#ifdef USE_DOUBLE_AMPLTIUDE
+      digitalWrite(PIN_IN2, HIGH);
+#endif
+      digitalWrite(PIN_ENABLE, HIGH);
+    }
+    else if (sigcode[step] == -1)
+    {
+      digitalWrite(PIN_IN1, HIGH);
+      digitalWrite(PIN_IN2, LOW);
+      digitalWrite(PIN_ENABLE, HIGH);
+    }
+    else
+    {
+      digitalWrite(PIN_ENABLE, LOW);
+    }
+    step++;
+    if (step == sizeof sigcode)
+    {
+      step = 0;
+    }
+  }
+  else
+  {
+    digitalWrite(PIN_ENABLE, LOW);
+  }
+}
+
+
+void readEEPROM()
+{
+  if (EEPROM.read(0) == 43)
+  {
+    // EEPROM data available
+    chargeADCZero = (EEPROM.read(1) << 8) | EEPROM.read(2);
+  }
+  else
+  {
+    Serial.println("no EEPROM data found, using default calibration (INA169)");
+  }
+  Serial.print("chargeADCZero=");
+  Serial.println(chargeADCZero);
+}
+
+
+void calibrateChargeCurrentSensor()
+{
+  Serial.println("calibrateChargeCurrentSensor "
+                 "(note: robot must be outside charging station!)");
+  RunningMedian<unsigned int, 32> measurements;
+  for (unsigned int i = 0; i < measurements.getSize(); i++)
+  {
+    unsigned int m = analogRead(PIN_CHARGE_CURRENT);
+    //Serial.println(m);
+    measurements.add(m);
+    delay(50);
+  }
+  float v;
+  measurements.getAverage(v);
+  chargeADCZero = v;
+  EEPROM.write(0, 43);
+  EEPROM.write(1, chargeADCZero >> 8);
+  EEPROM.write(2, chargeADCZero & 0xFF);
+  Serial.println("calibration done");
+  readEEPROM();
+}
+
+
+void checkKey()
+{
+  if (Serial.available() > 0)
+  {
+    char ch = (char)Serial.read();
+    Serial.print("received key=");
+    Serial.println(ch);
+    while (Serial.available())
+    {
+      Serial.read();
+    }
+    switch (ch)
+    {
+      case '1':
+        calibrateChargeCurrentSensor();
+        break;
+    }
+  }
+}
+
+
+// Interrupt service run when Timer/Counter2 reaches OCR2A
+//ISR(TIMER2_COMPA_vect) {
+//  if (digitalRead(pinFault) == LOW) fault = true;
+//if (digitalRead(pinPWM) == HIGH) fault = true;
+//fault = true;
+
+
+void fault()
+{
+  Serial.println("MC_FAULT");
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(PIN_LED, HIGH);
+    delay(50);
+    digitalWrite(PIN_LED, LOW);
+    delay(50);
+  }
+  faults++;
+}
+
+
+void setup()
+{
+  pinMode(PIN_IN1, OUTPUT);
+  pinMode(PIN_IN2, OUTPUT);
+  pinMode(PIN_ENABLE, OUTPUT);
+  pinMode(PIN_PWM, OUTPUT);
+
+  pinMode(PIN_FEEDBACK, INPUT);
+  pinMode(PIN_FAULT, INPUT);
+  pinMode(PIN_POT, INPUT);
+  pinMode(PIN_CHARGE_CURRENT, INPUT);
+
+  // configure ADC reference
+  // analogReference(DEFAULT); // ADC 5.0v ref
+  analogReference(INTERNAL); // ADC 1.1v ref
+
+  // sample rate 9615 Hz (19230,76923076923 / 2 => 9615.38)
+  int T = 1000.0 * 1000.0 / 9615.38;
+  Serial.begin(19200);
+
+  Serial.println("START");
+  Serial.print("Ardumower Sender ");
+  Serial.println(VER);
+#ifdef USE_DEVELOPER_TEST
+  Serial.println("Warning: USE_DEVELOPER_TEST activated");
+#endif
+  //Serial.println("press...");
+  //Serial.println("  1  for current sensor calibration");
+  //Serial.println();
+
+  readEEPROM();
+  Serial.print("T=");
+  Serial.println(T);
+  Serial.print("f=");
+  Serial.println(1000.0 * 1000.0 / T);
+  Timer1.initialize(T);         // initialize timer1, and set period
+  //Timer1.pwm(pinPWM, 256);
+  Timer1.attachInterrupt(timerCallback);
+  //digitalWrite(pinIN1, HIGH);
+  //digitalWrite(pinIN2, LOW);
+  //tone(pinPWM, 7800);
+
+  // http://playground.arduino.cc/Main/TimerPWMCheatsheet
+  // timer 2 pwm freq 31 khz
+  //cli();
+  TCCR2B = (TCCR2B & 0b11111000) | 0x01;
+  //TIMSK2 |= (1 << OCIE2A);     // Enable Output Compare Match A Interrupt
+  //OCR2A = 255;                 // Set compared value
+  //sei();
+}
+
+
+void loop()
+{
+  unsigned long curMillis;
+
+  curMillis = millis();
+  if (curMillis >= nextTimeControl)
+  {
+    nextTimeControl = curMillis + 100;
+    dutyPWM = 255;
+    if (isCharging)
+    {
+      // switch off perimeter
+      enableSender = false;
+    }
+    else
+    {
+      // switch on perimeter
+      enableSender = true;
+      //analogWrite(pinPWM, 255);
+      analogWrite(PIN_PWM, dutyPWM);
+      if (USE_PERI_FAULT
+          && dutyPWM == 255
+          && digitalRead(PIN_FAULT) == LOW)
+      {
+        enableSender = false;
+        dutyPWM = 0;
+        analogWrite(PIN_PWM, dutyPWM);
+        fault();
+      }
+    }
+  }
+
+  curMillis = millis();
+  if (curMillis >= nextTimeInfo)
+  {
+    nextTimeInfo = curMillis + 500;
+    checkKey();
+
+    //unsigned int v = 0;
+    float v = 0;
+    // determine charging current (Ampere)
+    if (USE_CHG_CURRENT)
+    {
+      chargeCurrentMeasurements.getAverage(v);
+      chargeCurrent = ((float)((int)v - (int)chargeADCZero)) / 1023.0 * 1.1;
+      isCharging = abs(chargeCurrent) >= CHG_CURRENT_MIN;
+    }
+
+    if (USE_PERI_CURRENT)
+    {
+      // determine perimeter current (Ampere)
+      periCurrentMeasurements.getAverage(v);
+      periCurrentAvg = (float)v / 1023.0 * 1.1 / 0.525;   // 525 mV per amp
+      unsigned int h;
+      periCurrentMeasurements.getHighest(h);
+      periCurrentMax = (float)h / 1023.0 * 1.1 / 0.525;   // 525 mV per amp
+    }
+
+    Serial.print("time=");
+    Serial.print(millis() / 1000);
+    Serial.print("\tchgCurrent=");
+    Serial.print(chargeCurrent, 3);
+    Serial.print("\tchgCurrentADC=");
+    chargeCurrentMeasurements.getAverage(v);
+    Serial.print(v);
+    Serial.print("\tisCharging=");
+    Serial.print(isCharging);
+    Serial.print("\tperiCurrent avg=");
+    Serial.print(periCurrentAvg);
+    Serial.print("\tmax=");
+    Serial.print(periCurrentMax);
+    Serial.print("\tdutyPWM=");
+    Serial.print(dutyPWM);
+    Serial.print("\tfaults=");
+    Serial.print(faults);
+    Serial.println();
+  }
+
+  if (USE_PERI_CURRENT)
+  {
+    periCurrentMeasurements.add(analogRead(PIN_FEEDBACK));
+  }
+
+  if (USE_CHG_CURRENT)
+  {
+    // determine charging current (Ampere)
+    chargeCurrentMeasurements.add(analogRead(PIN_CHARGE_CURRENT));
+  }
+
+  // LED status
+  if (isCharging)
+  {
+    // charging
+    curMillis = millis();
+    if (curMillis >= nextTimeToggleLED)
+    {
+      nextTimeToggleLED = curMillis + 500;
+      stateLED = !stateLED;
+    }
+  }
+  else
+  {
+    // not charging => indicate perimeter wire state (OFF=broken)
+    stateLED = periCurrentAvg >= PERI_CURRENT_MIN;
+  }
+  digitalWrite(PIN_LED, stateLED);
+}
