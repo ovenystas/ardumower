74c2d52cb7f7be2c464ec0a3f1eade0b18d7d250
Ove Nystås <ove.nystas@gmail.com>
2020-04-30 12:57:15 +0200

    Remove old Setting template

--- ArduMower/Setting.h
+++ ArduMower/Setting.h
@@ -9,24 +9,6 @@
 
 #include <Arduino.h>
 
-#if 0
-template <class T>
-struct Setting
-{
-  const String name;
-  const String unit;
-  T value;
-  const T minValue;
-  T maxValue;
-  float scale;
-
-  Setting(const String _name, const String _unit, T _value,
-      const T _minValue = 0, const T _maxValue = 0, const float _scale = 1.0 ) :
-        name(_name), unit(_unit), value(_value),
-        minValue(_minValue), maxValue(_maxValue), scale(_scale) {}
-};
-#endif
-
 template <class T>
 struct Setting
 {
